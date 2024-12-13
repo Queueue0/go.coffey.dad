98068fa27b460eb08a288f8416f4c262440d48ba
Queueue0 <jcoffey97@gmail.com>
2024-12-13 08:59:30 -0500

    re-order some model functions for better readability

--- internal/models/tags.go
+++ internal/models/tags.go
@@ -31,6 +31,23 @@ func (ts TagList) Contains(t Tag) bool {
 	return false
 }
 
+func (m *PostModel) GetTagByName(name string) (Tag, error) {
+	stmt := "SELECT id, name, color FROM tag WHERE name = ?"
+
+	var t Tag
+
+	err := m.DB.QueryRow(stmt, name).Scan(&t.ID, &t.Name, &t.Color)
+	if err != nil {
+		if errors.Is(err, sql.ErrNoRows) {
+			return Tag{}, ErrNoRecord
+		} else {
+			return Tag{}, err
+		}
+	}
+
+	return t, nil
+}
+
 func (m *PostModel) InsertTag(t Tag) (int, error) {
 	stmt := "INSERT INTO tag (name, color) VALUES (?, ?)"
 
@@ -60,6 +77,22 @@ func (m *PostModel) InsertTagIfNotExists(t Tag) (int, error) {
 	return et.ID, nil
 }
 
+func (m *PostModel) PostTagExists(p, t int) (bool, error) {
+	stmt := "SELECT id FROM post_tag WHERE post_id = ? AND tag_id = ?"
+
+	var id int
+	err := m.DB.QueryRow(stmt, p, t).Scan(&id)
+	if err != nil {
+		if err != sql.ErrNoRows {
+			return false, err
+		}
+
+		return false, nil
+	}
+
+	return true, nil
+}
+
 func (m *PostModel) InsertPostTag(p Post, t Tag) (int, error) {
 	stmt := "INSERT INTO post_tag (post_id, tag_id) VALUES (?, ?)"
 
@@ -76,23 +109,6 @@ func (m *PostModel) InsertPostTag(p Post, t Tag) (int, error) {
 	return int(id), nil
 }
 
-func (m *PostModel) GetTagByName(name string) (Tag, error) {
-	stmt := "SELECT id, name, color FROM tag WHERE name = ?"
-
-	var t Tag
-
-	err := m.DB.QueryRow(stmt, name).Scan(&t.ID, &t.Name, &t.Color)
-	if err != nil {
-		if errors.Is(err, sql.ErrNoRows) {
-			return Tag{}, ErrNoRecord
-		} else {
-			return Tag{}, err
-		}
-	}
-
-	return t, nil
-}
-
 func (m *PostModel) InsertPostTagIfNotExists(p Post, t Tag) error {
 	exists, err := m.PostTagExists(p.ID, t.ID)
 	if err != nil {
@@ -104,7 +120,6 @@ func (m *PostModel) InsertPostTagIfNotExists(p Post, t Tag) error {
 	}
 
 	_, err = m.InsertPostTag(p, t)
-	// err will be nil if the insert succeeds, so no real need to do a check here
 	return err
 }
 
@@ -122,22 +137,6 @@ func (m *PostModel) DeletePostTag(p, t int) (int, error) {
 	}
 
 	return int(rows), nil
-}
-
-func (m *PostModel) PostTagExists(p, t int) (bool, error) {
-	stmt := "SELECT id FROM post_tag WHERE post_id = ? AND tag_id = ?"
-
-	var id int
-	err := m.DB.QueryRow(stmt, p, t).Scan(&id)
-	if err != nil {
-		if err != sql.ErrNoRows {
-			return false, err
-		}
-
-		return false, nil
-	}
-
-	return true, nil
 }
 
 func (m *PostModel) AllTagsForPost(postId int) (TagList, error) {
