f9e11741520140a8bda8732595fb26dbacba1805
Queueue0 <jcoffey97@gmail.com>
2024-12-13 08:47:39 -0500

    Added ability to type tab character in body box of post editor

--- ui/static/js/add_edit.js
+++ ui/static/js/add_edit.js
@@ -168,6 +168,23 @@ document.addEventListener("DOMContentLoaded", () => {
   });
 
   document.getElementById("addTagBtn").addEventListener("click", add_tag);
+
+  // Make tab type a tab character in the body editor box
+  document.getElementById('bodybox').addEventListener('keydown', function(e) {
+    if (e.key == 'Tab') {
+      e.preventDefault();
+      var start = this.selectionStart;
+      var end = this.selectionEnd;
+
+      // set textarea value to: text before caret + tab + text after caret
+      this.value = this.value.substring(0, start) +
+        "\t" + this.value.substring(end);
+
+      // put caret at right position again
+      this.selectionStart =
+        this.selectionEnd = start + 1;
+    }
+  });
 });
 
 function handlePopupResult(result) {
